4310cc8045ddae1454aafe4c3373dcbf1f58d28f
nguyenhatiendat <dat.2474802010077@vanlanguni.vn>
2025-05-28 23:49:32 +0700

    update

--- Bài 1/Bài 1.2.2.py
+++
@@ -1,9 +0,0 @@
-def tao_danhsach():
-    return [100,200,300]
-#Reset giá trị trước
-a=b=c=0
-print("Trước khi gán lại:",a,b,c)
-#Gọi hàm và gán lại
-mylist=tao_danhsach()
-a,b,c=mylist
-print("Sau khi gán lại:",a,b,c)
--- Bài 2/Bài 2.2.1.py
+++
@@ -1,8 +0,0 @@
-from sympy import symbols, factor, expand
-x,y=symbols('x y')
-b1=x**3-y**3
-print("Biểu thức ban đầu:",b1)
-print("Biểu thức sau khi factor():",factor(b1))
-b2=(x-y)*(x**2+x*y+y**2)
-print("\nBiểu thức ban đầu:",b2)
-print("Biểu thức sau khi expand():",expand(b2))
